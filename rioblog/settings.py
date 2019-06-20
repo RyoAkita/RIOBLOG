93a1db0e3716fd24ddfa8fe09668897c62412c86
アキタリョウ <ryotennis0503@gmail.com>
2019-06-21 01:06:28 +0900

    settings for heroku deploy

--- rioblog/settings.py
+++ rioblog/settings.py
@@ -126,3 +126,14 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
